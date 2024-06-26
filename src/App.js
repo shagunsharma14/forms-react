1f5ee8d1fc75a1afc7461634be22961aa33407d6
shagunsharma14 <shaguns880@gmail.com>
2024-06-26 22:02:21 +0530

    text'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
+
+  const [text, setText] = useState("");
+  const onChangeHandler = (e)=>{
+      console.log(e.target.name, e.target.value);
+      setText(e.target.value);
+  }
+  return (      
     <div className="App">
-      
+      <h1>Text</h1>
+      <h3>{text}</h3>
+      <input className='form-control' onChange={onChangeHandler} name='text'/>
     </div>
   );
 }
