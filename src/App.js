0710d926dcb009971efe07f4441cf362d91ab91b
shagunsharma14 <shaguns880@gmail.com>
2024-06-27 01:10:21 +0530

    basic form handling

--- src/App.js
+++ src/App.js
@@ -3,17 +3,91 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    occupation: '',
+    gender: '',
+    languages:[],
+  })
 
-  const [text, setText] = useState("");
-  const onChangeHandler = (e)=>{
-      console.log(e.target.name, e.target.value);
-      setText(e.target.value);
+  const onChangehandler = (event)=>{
+    console.log(event);
+   if(event.target.name === 'languages'){
+    let copy = {...formData}
+    if(event.target.checked){
+      copy.languages.push(event.target.value)
+    }
+    else{
+      copy.languages = copy.languages.filter(el=> el !== event.target.value)
+    }
+    setFormData(copy)
+   }else{
+    setFormData(()=>({
+      ...formData,
+      [event.target.name]: event.target.value
+    }));
+   }
   }
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
+    console.table(formData);
+    console.log('Submit');
+  }
+
   return (      
     <div className="App">
-      <h1>Text</h1>
-      <h3>{text}</h3>
-      <input className='form-control' onChange={onChangeHandler} name='text'/>
+      <form onSubmit={onSubmitHandler}>
+        <div className='form-group'>
+          <label htmlFor='username' className='form-label'>User Name</label>
+          <input className='form-control' name='username' onChange={onChangehandler}/>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email' className='form-label'>Email</label>
+          <input className='form-control' name='email'  onChange={onChangehandler}/>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='occupation' className='form-label'>Occupation</label>
+          <select className='form-control' name='occupation'  onChange= {onChangehandler}>
+            <option value='student'>Student</option>
+            <option value='employee'>Employee</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='gender' className='form-label'>Gender</label>
+          <div className='form-check'>
+            <input  type='radio' name='gender' value='male'  onChange={onChangehandler}/>
+            <label htmlFor='male'>Male</label>
+          </div>
+          <div className='form-check'>
+            <input type='radio' name='gender' value='female'  onChange={onChangehandler}/>
+            <label htmlFor='female'>Female</label>
+          </div>
+          <div className='form-check'>
+            <input  type='radio' name='gender' value='other'  onChange={onChangehandler}/>
+            <label htmlFor='other'>Other</label>
+          </div>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='languages' className='form-label'>languages</label>
+          <div className='form-check'>
+            <input  type='checkbox' name='languages' value='html'  onChange={onChangehandler}/>
+            <label htmlFor='html'>HTML</label>
+          </div>
+          <div className='form-check'>
+            <input  type='checkbox' name='languages' value='CSS'  onChange={onChangehandler}/>
+            <label htmlFor='CSS'>CSS</label>
+          </div>
+          <div className='form-check'>
+            <input c type='checkbox' name='languages' value='javascript'  onChange={onChangehandler}/>
+            <label htmlFor='javascript'>javascript</label>
+          </div>
+        </div>
+        <div className='form-group'>
+          <button className='btn' type='submit'onClick={console.log(formData)}>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
